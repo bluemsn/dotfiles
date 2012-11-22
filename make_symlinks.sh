f79c695e3fd4e4935acdaee418089699056ae698
Eduan Lavaque <eduan@snapsimpletech.com>
2012-11-22 07:56:57 -0600

    Did zome changes to Vim, added Linux to script
    + Added functionality to use in Linux, make_symlinks.sh shell script
      that is
    + Added some bunldes to Vim: SearchComplete, ctrlp, delimitMate, and
      Tabularize
    +~ Improved the current script to be more friendly, and better
    ~ Did some changes to my filetype.vim file
    ~ Did some changes to my .vimrc file

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -7,30 +7,56 @@
 # Licenced under the MIT license (http://mit-license.org/)
 #####################
 
+kernel=`uname -s`        # Current Kernel name
+user=`whoami`            # Current Unix username
+time=`date +%H:%M`       # Output current time
 current_dir=$(pwd)       # Shell's current location
 script_dir=$(dirname $0) # This script's current location
 
 # This if statement is to know the correct location of the script, if the
 # shell's location is the same as the script's location
-if [ $script_dir = '.' ]; then
+if [ $script_dir == '.' ]; then
 	script_dir="$current_dir"
 fi
 
-# Making symlinks to Vim files, add yours as you need
-echo 'Making symlinks to Vim files'
-ln -s -v $script_dir/vim ~/.vim
-ln -s -v $script_dir/vim/vimrc ~/.vimrc
-ln -s -v $script_dir/vim/gvimrc ~/.gvimrc
-echo "Done...\n"
+if [ $kernel == 'Darwin' ]; then
+	# Making symlinks to Vim files, add yours as you need
+	echo 'Making symlinks to Vim files'
+	ln -s -v $script_dir/vim ~/.vim
+	ln -s -v $script_dir/vim/vimrc ~/.vimrc
+	ln -s -v $script_dir/vim/gvimrc ~/.gvimrc
+	echo "Done at [$time]...\n"
+	
+	# Making symlinks to Zsh files, add yours as you need
+	echo 'Making symlinks to Zsh files'
+	ln -s -v $script_dir/zsh ~/.zsh
+	ln -s -v $script_dir/zsh/zshrc ~/.zshrc
+	echo "Done at [$time]...\n"
 
-# Making symlinks to Zsh files, add yours as you need
-echo 'Making symlinks to Zsh files'
-ln -s -v $script_dir/zsh ~/.zsh
-ln -s -v $script_dir/zsh/zshrc ~/.zshrc
-echo "Done...\n"
+	# Making symlinks to Git files, add yours as you need
+	echo 'Making symlinks to Git files'
+	ln -s -v $script_dir/git/gitconfig ~/.gitconfig
+	ln -s -v $script_dir/git/gitmessage.txt ~/.gitmessage.txt
+	echo "Done at [$time]...\n"
+fi
 
-# Making symlinks to Git files, add yours as you need
-echo 'Making symlinks to Git files'
-ln -s -v $script_dir/git/gitconfig ~/.gitconfig
-ln -s -v $script_dir/git/gitmessage.txt ~/.gitmessage.txt
-echo "Done...\n"
+if [ $kernel == 'Linux' ]; then
+	# Making symlinks to Vim files, add yours as you need
+	echo 'Making symlinks to Vim files'
+	ln -s -v $script_dir/vim /home/$user/.vim
+	ln -s -v $script_dir/vim/vimrc /home/$user/.vimrc
+	ln -s -v $script_dir/vim/gvimrc /home/$user/.gvimrc
+	echo "Done at [$time]...\n"
+
+	# Making symlinks to Zsh files, add yours as you need
+	echo 'Making symlinks to Zsh files'
+	ln -s -v $script_dir/zsh /home/$user/.zsh
+	ln -s -v $script_dir/zsh/zshrc /home/$user/.zshrc
+	echo "Done at [$time]...\n"
+
+	# Making symlinks to Git files, add yours as you need
+	echo 'Making symlinks to Git files'
+	ln -s -v $script_dir/git/gitconfig /home/$user/.gitconfig
+	ln -s -v $script_dir/git/gitmessage.txt /home/$user/.gitmessage.txt
+	echo "Done at [$time]...\n"
+fi
--- vim/filetype.vim
+++ vim/filetype.vim
@@ -1,9 +1,17 @@
-" Add Markdown syntax
+" If filetypes were already loaded, don't continue
 if exists("did_load_filetypes")
 	finish
 endif
+
+" Detect some Markdown fileytypes
 augroup filetypedetect
 	au! BufRead,BufNewFile *.md setfiletype markdown
-"   au! BufRead,BufNewFile *.mdown setfiletype markdown
-"   au! BufRead,BufNewFile *.markdown setfiletype markdown
+augroup END
+
+augroup filetypedetect
+	au! BufRead,BufNewFile *.mdown setfiletype markdown
+augroup END
+
+augroup filetypedetect
+	au! BufRead,BufNewFile *.markdown setfiletype markdown
 augroup END
