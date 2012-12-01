58b9f0ef7d479a48eab091d0d31bfc8b81539a86
Eduan Lavaque <eduan@snapsimpletech.com>
2012-11-30 18:55:16 -0600

    Fixed setup_vim.sh shell script, changes to vimrc
    +~ Fixed the file header format for the setup_vim.sh file
    +~ Improved setup_vim.sh script
    ~ Moved the setup_vim.sh script to the scripts folder
    ~ Changes to vimrc

---
+++ scripts/setup_vim.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# =============================================================================
+# setup_vim.sh
+# -----------------------------------------------------------------------------
+# This script will do a couple of things for you that you would normally do
+# when installing MacVim in a new Mac
+# =============================================================================
+# Eduan Lavaque <eduan@snapsimpletech.com>
+# Licensed under the MIT license (htpp://eduan.mit-license.org/)
+# =============================================================================
+
+# Fix a Python error that you get when using 'vim' in the Terminal
+sudo mkdir -p /usr/include/python2.7
+sudo ln -s /System/Library/Frameworks/Python.framework/Versions/Current/include/python2.7/pyconfig.h /usr/include/python2.7
+
+# Install Vundle and all it's bundles
+git clone git://github.com/gmarik/vundle.git ./bundle/vundle
+vim +BundleInstall! +BundleClean +qa
