e083d81ab7859a6f5a96c6fe1636f0f2271b8b57
Eduan Lavaque <eduan@websharks-inc.com>
2013-01-13 11:00:44 -0500

    Added a new SSH key generator
    It just generates the key for you and puts it in your clipboard.

---
+++ scripts/ssh_key.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+# =============================================================================
+# brew_installs.sh
+# -----------------------------------------------------------------------------
+# This file will automatically setup a SSH key for you with GitHub. Creating it
+# in your OS and making GitHub accept it as well.
+# =============================================================================
+# Eduan Lavaque <eduan@snapsimpletech.com>
+# Licensed under the MIT license (http://eduan.mit-license.org/)
+# =============================================================================
+
+# Make sure the .ssh directory exists
+if [ ! -d ~/.ssh ]; then
+    # Create it if it doesn't exists
+    mkdir -v ~/.ssh
+elif [ -d ~/.ssh ]; then
+    # If it does exist then make a backup of all the files in it
+    mkdir -v ~/.ssh/key_backup
+    cp id_rsa* key_backup
+    rm id_rsa*
+fi
+
+# Generate a SSH key
+ssh-keygen -t rsa -C "eduan@snapsimpletech.com"
+
+# Make sure to give it the correct directory...
+# Enter the passphrase twice...
+
+# Copy the SSH key to my clipboard
+pbcopy < ~/.ssh/id_rsa.pub
