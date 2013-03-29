6520c8b8ac32f610058a08db682a1cf2c3e1bd45
Eduan Lavaque <eduan@snapsimpletech.com>
2013-03-28 22:45:17 -0400

    Finished(?) and perfected symlink maker...

--- scripts/symlinks.py
+++ scripts/symlinks.py
@@ -1,4 +1,4 @@
-git#!/usr/bin/python2
+#!/usr/bin/python2
 # symlinks.py by Eduan Lavaque <eduan@snapsimpletech.com>
 # Licensed under the MIT license (http://eduan.mit-license.org/)
 # This file will generate my symlinks. For new installs.
@@ -15,7 +15,7 @@ if not os.path.exists(backup_main_folder):
 	os.mkdir(backup_main_folder, 0755)
 
 # Get the current time in a "YYYY-MM-DD" format
-date = strftime('%Y-%m-%d', gmtime())
+date = strftime('%Y-%m-%d %H:%M', gmtime())
 
 # Backup folder
 backup_folder = backup_main_folder + '/' + date
@@ -24,33 +24,45 @@ backup_folder = backup_main_folder + '/' + date
 if not os.path.exists(backup_folder):
 	os.mkdir(backup_folder, 0755)
 
-# Tuple with all the locations of the files from $DOTROOT (in a manner of speech)
-source = ['git/gitmessate.txt', 'shells/bash', 'shells/bash/bash_profile', 'shells/bash/bashrc', 'shells/zsh', 'shells/zsh/zshrc', 'shells/profile', 'tmux/tmux.conf', 'vim', 'vim/gvimrc.vim', 'vim/vimrc.vim']
-# Tuple with all the destinations of the files in the dotfiles
-destination = ['.gitmessage.txt', '.bash', '.bash_profile', '.bashrc', '.zsh', '.zshrc', '.profile', '.tmux.conf', '.vim', '.gvimrc', '.vimrc']
+# Set a definite $DOTROOT sorta variable
+dotroot = expanduser('~') + '/dotfiles'
 
-for src in source:
-	for dest in destination:
-		# If the file exists but it's a symlink...
-		if os.path.exists(dest) and os.path.islink(dest):
-			# Then delete it
-			os.remove(dest)
-		# If the file exists and it's NOT a symlink...
-		elif os.path.exists(dest) and not os.path.islink(dest):
-			# Figure out the backup destination
-			backup_dest = backup_folder + dest
-			# Move the file as a backup to the backup destination
-			os.rename(dest, backup_dest)
+# On the left side, the source (from $DOTROOT). On the right side, the destination (from $HOME)
+files = {
+	'/git/gitmessage.txt': '.gitmessage.txt',
+	'/shells/bash': '.bash',
+	'/shells/bash/bash_profile': '.bash_profile',
+	'/shells/bash/bashrc': '.bashrc',
+	'/shells/zsh': '.zsh',
+	'/shells/zsh/zshrc': '.zshrc',
+	'/shells/profile': '.profile',
+	'/tmux/tmux.conf': '.tmux.conf',
+	'/vim': '.vim',
+	'/vim/gvimrc.vim': '.gvimrc',
+	'/vim/vimrc.vim': '.vimrc',
+}
 
-		# The actual source that's going to be used
-		final_src = expanduser('~') + '/dotfiles/' + src
-		# The actual destination that's going to be used
-		final_dest = expanduser('~') + dest
+for src, dest in files.iteritems():
+	# If the file exists but it's a symlink...
+	if os.path.exists(dest) and os.path.islink(dest):
+		# Then delete it
+		os.remove(dest)
+	# If the file exists and it's NOT a symlink...
+	elif os.path.exists(dest) and not os.path.islink(dest):
+		# Figure out the backup destination
+		backup_dest = backup_folder + dest
+		# Move the file as a backup to the backup destination
+		os.rename(dest, backup_dest)
 
-		# Make symlink
-		os.symlink(final_src, final_dest)
+	# The actual source that's going to be used
+	final_src = dotroot + src
+	# The actual destination that's going to be used
+	final_dest = expanduser('~') + dest
 
-		# Print message regarding currently symlink...
-		print '%s -> %s' % (final_dest, final_src)
+	# Make symlink
+	os.symlink(final_src, final_dest)
+
+	# Print message regarding currently symlink...
+	print '%s -> %s' % (final_dest, final_src)
 
 print 'Finished making symlinks!'
