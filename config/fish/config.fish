30802696cd43b7604b412fdcf1c93ada624bf5b9
Greduan <eduanlavaque@gmail.com>
2013-11-13 11:51:21 -0600

    Changed little thing in Fish shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,7 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_char_dirtystate '+'
 set __fish_git_prompt_char_stagedstate '*'
 set __fish_git_prompt_char_stashstate '$'
+set __fish_git_prompt_char_cleanstate 'OK'
 set __fish_git_prompt_char_upstream_ahead 'a'
 set __fish_git_prompt_char_upstream_behind 'b'
 
